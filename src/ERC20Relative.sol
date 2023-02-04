266809177d3ed0f8b3a629f57da6626f2529baf1
shafu.eth <selfouly@gmail.com>
2023-02-04 20:09:56 +0100

    setup

--- src/ERC20Relative.sol
+++ src/ERC20Relative.sol
@@ -1,14 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract Counter {
-    uint256 public number;
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-    function setNumber(uint256 newNumber) public {
-        number = newNumber;
-    }
+contract ERC20Relative is ERC20 {
+  constructor (string memory name, string memory symbol) ERC20(name, symbol) {}
 
-    function increment() public {
-        number++;
-    }
+  function balanceOf(address account) public view override returns (uint256) {
+      return 0;
+  }
 }
